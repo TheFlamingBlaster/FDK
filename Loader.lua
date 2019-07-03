429b46a1735f923daf9fddb47f19a21fdd45526b
theflamingblaster <theflamingblaster@gmail.com>
2019-07-03 15:16:44 -0300

    Fixed the loader.

--- Loader.lua
+++ Loader.lua
@@ -4,11 +4,13 @@
 	TheFlamingBlaster, 2019
 	Licenced under the terms at: https://www.apache.org/licenses/LICENSE-2.0.txt
 --]]
+local hs = game:GetService("HttpService")
 
 local classPath = "https://raw.githubusercontent.com/TheFlamingBlaster/FDK/master/BaseClass.lua" -- Where the base class will be loaded from. 
 local fdkPath = "https://raw.githubusercontent.com/TheFlamingBlaster/FDK/master/FDK.lua" -- Where FDK is loaded from.
 
-baseClass = loadstring(classPath)()
-fdk = loadstring(fdkPath)()
+baseClass = loadstring(hs:GetAsync(classPath))()
+fdk = loadstring(hs:GetAsync(fdkPath))
+setfenv(fdk, getfenv())
 
-return fdk
+return fdk()
